8192023f3dd597272ce6e1286097e36fd69b8dad
46Mike <59586737+46Mike@users.noreply.github.com>
2024-12-15 00:53:32 +0100

    Update home-test.php

--- home-test.php
+++ home-test.php
@@ -1,14 +1,1 @@
-<?php
 
-$dbservername = 'mytrucv121.mysql.db';
-$dbusername = 'mytrucv121';
-$dbpassword = '_78.DantE.24_';
-$dbname = 'mytrucv121';
-
-$conn = mysqli_connect ($dbservername, $dbusername, $dbpassword, $dbname);
-if ($conn = TRUE) 
-     {
-          echo 'connesso';
-     }
-
-?>
